af9a088303793fb286c81a26b76c0dab8154c53f
Gabriela Karkowska <karkowskagabriela@gmail.com>
2024-06-19 20:52:06 +0200

    return exercise

---
+++ fundamentals.py
@@ -0,0 +1,38 @@
+# Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+# Please use the following function names:
+
+# addition = add
+
+# multiply = multiply
+
+# division = divide (both integer and float divisions are accepted)
+
+# modulus = mod
+
+# exponential = exponent
+
+# subtraction = subt
+
+# Note: All math operations will be: a (operation) b
+
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print("division by zero cannot be conducted.")
+
+def mod(a, b):
+    return a % b
+
+def exponent(a, b):
+    return a ** b
+
+def subt(a, b):
+    return a - b
