b59533a04358b1739a2a104a275271dadc255486
Gabriela Karkowska <karkowskagabriela@gmail.com>
2024-06-24 18:22:33 +0200

    pig latin + punc exercise

---
+++ pig_punctuation.py
@@ -0,0 +1,16 @@
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+# pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+# pig_it('Hello world !')     # elloHay orldway !
+
+def pig_it(text):
+    words = text.split()
+    pig_words = []
+    for word in words:
+        if word.isalpha():
+            pig_word = word[1:] + word[0] + "ay"
+            pig_words.append(pig_word)
+        else:
+            pig_word = word
+            pig_words.append(pig_word)
+    return " ".join(pig_words)    
