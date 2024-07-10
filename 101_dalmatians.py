6413aa2b71081a9aabe1f3d8fda38a19fedba9a3
Gabriela Karkowska <karkowskagabriela@gmail.com>
2024-07-10 12:13:01 +0200

    dalmatians exercise

---
+++ 101_dalmatians.py
@@ -0,0 +1,15 @@
+# Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+
+# By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+
+# The number of dogs will always be a number and there will always be at least 1 dog.
+
+def how_many_dalmatians(n):  
+    if n <= 10:
+        return "Hardly any"
+    elif n <= 50:
+        return "More than a handful!"
+    elif n == 101:
+        return "101 DALMATIONS!!!"
+    else:
+        return "Woah that's a lot of dogs!"
